d2fee01eb1fa2701e1812defaf61a753559d8933
Nareekarn <n.phthak@gapp.nthu.edu.tw>
2019-12-14 15:12:28 +0800

    language and provider file

---
+++ scripts/provider.py
@@ -0,0 +1,34 @@
+import json
+from neo4j import GraphDatabase, basic_auth
+
+# Load and parse data
+file = open('data/adb_courses.json', "rb", buffering=0)
+data = json.load(file)
+
+# Create graph driver
+# This is used to create a session so we can run the code while working on it.
+driver = GraphDatabase.driver('bolt://localhost:7687',
+                              auth=basic_auth('neo4j',  'password'))
+
+
+def add_provider(driver):
+    """Add the Provider information to the database"""
+    # Create session
+    session = driver.session()
+
+    # Get unique providers
+    providers = list(set(data['Provider'].values()))
+
+    # Create provider nodes
+    for provider in providers:
+        if provider in providers:
+            print('Provider already existed')
+            break
+        else:
+            session.run('CREATE (p:Provider {provider: $provider }) RETURN p', provider=provider)
+            print('Providers added were: ', add_provider(driver))
+    session.close()
+
+    return providers
+
+print('Current providers: ', add_provider(driver))
