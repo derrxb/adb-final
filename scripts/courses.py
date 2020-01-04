1cb6e86563904f360f9774a24b4a177088fb4c91
Derrick Bol <derrickbol33@gmail.com>
2020-01-04 14:01:59 +0800

    Add difficulty level and link to courses

--- scripts/courses.py
+++ scripts/courses.py
@@ -1,5 +1,6 @@
 import json
 from neo4j import GraphDatabase, basic_auth
+from helpers import contains
 
 # Load and parse data
 file = open('data/adb_courses.json', "rb", buffering=0)
@@ -27,6 +28,61 @@ def add_course(driver, course_id):
     }
 
     # Add the course to the database
-    result = session.run('CREATE (c:Course $course) RETURN c', course=course)
+    result = session.run(
+        'CREATE (c:Course $course) RETURN c', course=course).single()[0]
 
-    return result.single()
+    session.close()
+
+    add_course_level(result, driver)
+
+    return result
+
+
+def create_course_levels(driver):
+    """Creates course difficulties levels"""
+    session = driver.session()
+
+    levels = ['neutral', 'basic', 'intermediate', 'advanced']
+
+    for level in levels:
+        session.run("CREATE (l:Level {name: $name })", name=level)
+
+    session.close()
+
+
+def add_course_level(result, driver):
+    """Links a course to a certain `difficulty` level"""
+    session = driver.session()
+    course = dict(result.items())
+    level = 'neutral'
+
+    title = course['title'].lower()
+
+    if contains(title, ['basic', 'essential', 'fundamental', 'introduction']):
+        level = 'basic'
+
+    if contains(title, ['intermediate']):
+        level = 'intermediate'
+
+    if contains(title, ['advanced']):
+        level = 'advanced'
+
+    if course['description'] is not None and level == 'neutral':
+        description = course['description'].lower()
+
+        if contains(description, ['basic', 'essential', 'fundamental', 'introduction']):
+            if not contains(description, ['advanced']):
+                level = 'basic'
+            elif contains(description, ['intermediate']):
+                level = 'intermediate'
+            elif contains(description, ['advanced']):
+                level = 'advanced'
+
+    query = '''MATCH (source:Level), (target:Course)
+               WHERE source.name = $level AND target.course_id = $course_id
+               CREATE (target)-[r:DIFFICULTY]->(source)
+               RETURN type(r)'''
+
+    session.run(query, level=level, course_id=course['course_id'])
+
+    session.close()
