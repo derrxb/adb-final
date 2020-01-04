56e1bc42ce6244232f900d2b49e89ababc725f15
Derrick Bol <derrickbol33@gmail.com>
2020-01-04 12:27:15 +0800

    Add course authors to the seed

--- scripts/seed.py
+++ scripts/seed.py
@@ -11,6 +11,7 @@ from add_course_weeks_section import add_course_weeks_section
 from add_course_lang import add_course_lang
 from add_course_keywords import add_course_keywords
 from add_course_provider import add_course_provider
+from add_course_authors import add_course_authors
 from users import create_users
 
 file = open('data/adb_courses.json', "rb", buffering=0)
@@ -38,6 +39,7 @@ def seed_data(driver):
     add_course_lang(driver)
     add_course_keywords(driver)
     add_course_provider(driver)
+    add_course_authors(driver)
 
     create_users(driver)
 
