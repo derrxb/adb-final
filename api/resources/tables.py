a16c9cdb5a8bbd5224af2ece70b874e310744a70
kharryyanto <kharryyanto@gmail.com>
2020-01-05 15:55:19 +0800

    Added login functionality and user course history

--- api/resources/tables.py
+++ api/resources/tables.py
@@ -12,3 +12,18 @@ class Results(Table):
 	tags = Col('Tags')
 	language = Col('Language')
 	provider = Col('Provider')
+    
+class History(Table):
+    # classes = ['table', 'table-bordered', 'table-striped']
+    photo_link = Col('Photo URL', show=False)
+    course_id = Col('Course ID', show=False)
+    title = Col('Title')
+    description = Col('Description')
+    direct_link = Col('URL')
+    authors = Col('Authors')
+    tags = Col('Tags')
+    language = Col('Language')
+    provider = Col('Provider')
+    enrollment_date = Col('Enrollment Date')
+    completion_date = Col('Completion Date')
+    status = Col('Status')
--- api/runserver.py
+++ api/runserver.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, g, Blueprint, render_template, request, redirect, flash
+from flask import Flask, g, Blueprint, render_template, request, redirect, flash, url_for, escape, session
 from flask_restful import Api
 from flask_cors import CORS
 from .resources.Author import AuthorResource
@@ -9,9 +9,11 @@ from api import app
 from py2neo import ogm
 from flask2neo4j import Flask2Neo4J
 from .resources.forms import CourseSearchForm
-from .resources.tables import Results
+from .resources.tables import Results, History
+#import flask_login
 
 from api.models.course import Course
+from api.models.user import User
 from api.common.cypher_helpers import page_number, page_size
 
 # Wrap app with API to enable Flask-REST API
@@ -23,13 +25,27 @@ api.add_resource(AuthorResource, '/authors/<int:id>')
 api.add_resource(CoursesResource, '/courses')
 app.register_blueprint(api_bp)
 
-# home
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# =============================================================================
+# login_manager = flask_login.LoginManager()
+# 
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get(user_id)
+# =============================================================================
+
 @app.route('/', methods=['GET','POST'])
 def index():
+    if 'username' in session:
+        flash('Logged in as %s' % escape(session['username']))
+        login = True
+    else:
+        login = False
     search = CourseSearchForm(request.form)
     if request.method == 'POST':
         return search_results(search)
-    return render_template('index.html', form=search)
+    return render_template('index.html', form=search, login=login)
 
 #@app.route('/results')
 def search_results(search):
@@ -62,7 +78,41 @@ def search_results(search):
 def explore():
 	return render_template('explore.html')	
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        login_validation = User().check_password(request.form['username'], request.form['password'])
+        if not login_validation:
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/history')
+def history():
+    # Use James510 as username for "good" results
+    error = None
+    try:
+        if session['username']:
+            results = User().find_history(session['username'], page_number(), page_size())
+            table = History(results)
+            table.border = True
+            return render_template('history.html', error=error, table=table)
+    except:
+        error = 'Not logged in'
+        return render_template('history.html', error=error)
+#    if session['username'] == None:
+#        error = 'Not logged in'
+#    return render_template('login.html', error=error)
+        
 print(app.url_map)
 
 # Enable debugging mode for dev environments
